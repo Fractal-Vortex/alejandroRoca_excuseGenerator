f094aafbad9655d73e06e8d56f0d92b97c36e453
Alejandro <64385399+Fractal-Vortex@users.noreply.github.com>
2024-08-01 13:53:40 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My Cat", "My Bird"];
+  let action = ["ate", "peed", "crushed", "broke", "blow"];
+  let what = ["my homework", "my phone", "the car", "my guitar", "laptop"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "When I was driving",
+  ];
+
+  function printExcuse() {
+    document.getElementById("excuse").innerHTML =
+      who[Math.floor(Math.random() * 5)] +
+      " " +
+      action[Math.floor(Math.random() * 5)] +
+      " " +
+      what[Math.floor(Math.random() * 5)] +
+      " " +
+      when[Math.floor(Math.random() * 5)] +
+      ".";
+  }
+
+  console.log(printExcuse());
 };
